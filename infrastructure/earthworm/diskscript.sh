694dae561ec906250b4b953e88728a2d10732336
Kristian Kjerstad <69512295+KristianKjerstad@users.noreply.github.com>
2021-08-31 09:31:54 +0200

    update-docs-for-infrastructure (#58)

---
+++ infrastructure/earthworm/diskscript.sh
@@ -0,0 +1,20 @@
+DISK_NAME=/dev/sdb
+DISK_PERCENTAGE_USED=$(df -Ph $DISK_NAME | grep -Po '\d+(?=%)')
+DISK_PERCENTAGE_LIMIT=80
+
+SERVICES=(earthworm seiscomp)
+echo "Disk percentage used: ${DISK_PERCENTAGE_USED}"
+if [ $DISK_PERCENTAGE_USED -gt $DISK_PERCENTAGE_LIMIT ]
+then
+    echo "Disk is almost full - moving data from folder /data/archive from local disk to azureFiles (/archive folder)."
+
+    # SHUT DOWN STREAM SERVICES
+    sudo systemctl stop ${SERVICES[@]}
+
+    # MOVE FILES FROM SDS DISK TO AZURE FILES AND DELETE MSEED FILES FROM /ccs-passive/mseed
+    sudo rsync --remove-source-files -a /data/archive /archive
+
+    # RESTART STREAM SERVICES
+    sudo systemctl start ${SERVICES[@]}
+
+fi
