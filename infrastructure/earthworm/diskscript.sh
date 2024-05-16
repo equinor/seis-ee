12c0cd08574551f5aa937d09dffdf295cc95ddd3
Stig Ofstad <stigofstad@gmail.com>
2024-05-16 09:26:43 +0200

    add date logging to email script

--- infrastructure/earthworm/diskscript.sh
+++ infrastructure/earthworm/diskscript.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env bash
+#set -euox pipefail
 DISK_NAME=/dev/sdb
 DISK_PERCENTAGE_USED=$(df -Ph $DISK_NAME | grep -Po '\d+(?=%)')
 DISK_PERCENTAGE_LIMIT=80
@@ -8,18 +9,21 @@ SERVICES=(earthworm seiscomp)
 echo "Disk percentage used: ${DISK_PERCENTAGE_USED}"
 if [ $DISK_PERCENTAGE_USED -gt $DISK_PERCENTAGE_LIMIT ]
 then
-    echo "Disk is almost full - moving data from folder /data/archive from local disk to azureFiles (/archive folder)."
+echo "Disk is almost full - moving data from folder /data/archive from local disk to azureFiles (/archive folder)."
 
-    # SHUT DOWN STREAM SERVICES
-    sudo systemctl stop ${SERVICES[@]}
+# SHUT DOWN STREAM SERVICES
+sudo systemctl stop ${SERVICES[@]}
 
-    # MOVE FILES FROM SDS DISK TO AZURE FILES AND DELETE MSEED FILES FROM /ccs-passive/mseed
-    sudo rsync --remove-source-files -a /data/archive /archive
+# MOVE FILES FROM SDS DISK TO AZURE FILES AND DELETE MSEED FILES FROM /ccs-passive/mseed
+sudo rsync --remove-source-files -a /data/archive /archive
 
-    # RESTART STREAM SERVICES
-    sudo systemctl start ${SERVICES[@]}
+# RESTART STREAM SERVICES
+sudo systemctl start ${SERVICES[@]}
 
 fi
 
 # Remove log files older than 10 days
-find "$LOG_FILES_DIR" -mtime +10 -type f -delete
+find "$LOG_FILES_DIR" -mtime +5 -type f -delete
+# Remove earthworm startstop logs(They can grow huge...)
+rm -rf /data/ew/logs/startstop.log
+rm -rf /data/ew/logs/startstop_error.log
--- send-ew-event-mail.sh
+++ send-ew-event-mail.sh
@@ -6,19 +6,26 @@ set -u
 WATCHDIR="/home/earthworm/ew/earthworm_7.10/run/data/web/ewhtmlemail"
 MAIL_HEADER="/home/earthworm/ew/earthworm_7.10/run/data/web/ewhtmlemail/em_header.tmp"
 
-echo "Script startet $(date)"
+log_prefix() {
+    LEVEL=${1:-"INFO"}
+    echo "$(date --rfc-3339 s) - $LEVEL:"
+}
+
+trap "echo '$(log_prefix)  Shutting down...'" EXIT
+
+echo "$(log_prefix) Script startet"
 
 inotifywait -m $WATCHDIR -e create -e moved_to |
     while read dir action file; do
         if [[ "$file" =~ ^(em_).*(.html) ]]; then
-            echo "The file '$file' appeared in directory '$dir' via '$action'"
-            echo "Sending email with command 'cat $MAIL_HEADER $WATCHDIR/$file | /usr/sbin/sendmail -t'..."
+            echo "$(log_prefix) The file '$file' appeared in directory '$dir' via '$action'"
+            echo "$(log_prefix) Sending email with command 'cat $MAIL_HEADER $WATCHDIR/$file | /usr/sbin/sendmail -t'..."
             cat $MAIL_HEADER $WATCHDIR/$file | /usr/sbin/sendmail -t
             if [ $? == 0 ]; then
-                echo "INFO: Email sent successfully"
+                echo "$(log_prefix) Email sent successfully"
             else
-                echo "ERROR: Failed to sent last email"
+                echo "$(log_prefix 'ERROR') Failed to sent last email"
             fi
-            echo "INFO: Watching for new files..."
+            echo "$(log_prefix) Watching for new files..."
         fi
     done
